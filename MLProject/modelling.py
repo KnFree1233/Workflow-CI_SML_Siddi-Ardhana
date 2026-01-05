954dcf4b399e912167f13d6d82441856ceb1af46
Siddi Ardhana <siddiardhana@gmail.com>
2026-01-05 16:39:03 +0700

    Add MLProject text for mlflow run and updtae modelling.py

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -6,38 +6,52 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 import os
+import argparse
 
-curr_dir = Path(__file__).parent.absolute()
-dataset_path = curr_dir / "covid19-patient-symptoms-diagnosis_preprocessing.csv"
-df = pd.read_csv(dataset_path)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("n_estimators", type=int)
+    parser.add_argument("max_depth", type=int)
+    parser.add_argument("dataset", type=str)
 
-X = df.drop('covid_result', axis=1)
-y = df['covid_result']
+    args = parser.parse_args()
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+    curr_dir = Path(__file__).parent.absolute()
+    dataset_path = curr_dir / args.dataset
+    df = pd.read_csv(dataset_path)
 
-mlflow_path = curr_dir / "mlruns"
+    X = df.drop('covid_result', axis=1)
+    y = df['covid_result']
 
-if os.name == 'nt':  # Windows
-    tracking_uri = mlflow_path.as_uri()
-else:  # Linux (GitHub Actions)
-    tracking_uri = f"file://{mlflow_path.as_posix()}"
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
-mlflow.set_tracking_uri(tracking_uri)
-mlflow.set_experiment("Covid19 Patient Symptoms Diagnosis Github")
-mlflow.sklearn.autolog(log_input_examples=True)
+    if not os.environ.get("MLFLOW_TRACKING_URI"):
+        mlflow_path = curr_dir / "mlruns"
+        if os.name == 'nt':  # Windows
+            tracking_uri = mlflow_path.as_uri()
+        else:  # Linux (GitHub Actions)
+            tracking_uri = f"file://{mlflow_path.as_posix()}"
+        mlflow.set_tracking_uri(tracking_uri)
 
-with mlflow.start_run():
-    rf = RandomForestClassifier(random_state=42, max_depth=12, n_estimators=32)
+    mlflow.sklearn.autolog(log_input_examples=True)
 
-    rf.fit(X_train, y_train)
-    predictions = rf.predict(X_test)
-    acc = accuracy_score(y_test, predictions)
+    with mlflow.start_run(nested=True):
+        rf = RandomForestClassifier(random_state=42, max_depth=args.max_depth, n_estimators=args.n_estimators)
 
-    metrics = {
-        "accuracy": acc,
-        "precision": precision_score(y_test, predictions, average='weighted'),
-        "recall": recall_score(y_test, predictions, average='weighted'),
-        "f1_score": f1_score(y_test, predictions, average='weighted')
-    }
-    mlflow.log_metrics(metrics)
+        rf.fit(X_train, y_train)
+        predictions = rf.predict(X_test)
+        acc = accuracy_score(y_test, predictions)
+
+        metrics = {
+            "test_accuracy": acc,
+            "test_precision": precision_score(y_test, predictions, average='weighted'),
+            "test_recall": recall_score(y_test, predictions, average='weighted'),
+            "test_f1_score": f1_score(y_test, predictions, average='weighted')
+        }
+        mlflow.log_metrics(metrics)
+
+        if "GITHUB_OUTPUT" in os.environ:
+            run = mlflow.active_run()
+            run_id = run.info.run_id
+            with open(os.environ["GITHUB_OUTPUT"], "a") as f:
+                f.write(f"run_id={run_id}\n")
